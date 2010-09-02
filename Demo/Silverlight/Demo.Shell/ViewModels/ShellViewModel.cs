66428cd3e04bdd08e9eb4eea5465e70e2c86ace7
wangxuesong <wraithkings@gmail.com>
2010-09-02 16:26:42 +0800

    add ShellView & ShellViewModel by Blend

---
+++ Demo/Silverlight/Demo.Shell/ViewModels/ShellViewModel.cs
@@ -0,0 +1,36 @@
+﻿namespace Demo.Shell.ViewModels
+{
+    using System;
+    using System.ComponentModel;
+
+    public class ShellViewModel : INotifyPropertyChanged
+    {
+        private string _name = "Test";
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                NotifyPropertyChanged("Name");
+            }
+        }
+
+        public ShellViewModel()
+        {
+            
+        }
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(String info)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
+        #endregion
+    }
+}
---
+++ Demo/Silverlight/Demo.Shell/Views/ShellView.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace Demo.Shell.Views
+{
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for ShellView.xaml
+    /// </summary>
+    public partial class ShellView : UserControl
+    {
+        public ShellView()
+        {
+            this.InitializeComponent();
+            
+            // Insert code required on object creation below this point.
+        }
+    }
+}
