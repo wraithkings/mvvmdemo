490a582fd92b795ffa8f1ee6d196214277584bee
wangxuesong <wraithkings@gmail.com>
2010-09-02 14:31:33 +0800

    add Silverlight & WPF project

---
+++ Demo/Silverlight/Demo.Shell/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Demo.Shell
+{
+    using System.Windows.Controls;
+
+    public partial class MainPage : UserControl
+    {
+        public MainPage()
+        {
+            // Required to initialize variables
+            InitializeComponent();
+        }
+    }
+}
---
+++ Demo/WPF/Demo.Shell/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace Demo.Shell
+{
+    using System.Windows;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+
+            // Insert code required on object creation below this point.
+        }
+    }
+}
