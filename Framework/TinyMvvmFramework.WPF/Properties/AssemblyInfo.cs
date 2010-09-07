970e5beeb0c2b8a48386a48bd52dda273c00905c
wangxuesong <wraithkings@gmail.com>
2010-09-07 21:04:56 +0800

    add xmlnamespace

--- Framework/TinyMvvmFramework.WPF/Properties/AssemblyInfo.cs
+++ Framework/TinyMvvmFramework.WPF/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Windows.Markup;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -34,3 +35,6 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: XmlnsDefinition(@"http://github.com/wraithkings/mvvmdemo", "TinyMvvmFramework")]
+[assembly: XmlnsPrefix("http://github.com/wraithkings/mvvmdemo", "tiny")]
